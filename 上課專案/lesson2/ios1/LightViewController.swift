e44f3392074fa98e3b971b32614fc0810bba220e
徐國堂 <roberthus2003@gmail.com>
2019-04-15 21:16:49 +0800

    modify

---
+++ 上課專案/lesson2/ios1/LightViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LightViewController.swift
+//  ios1
+//
+//  Created by Robert on 2019/4/15.
+//  Copyright © 2019 Robert. All rights reserved.
+//
+
+import UIKit
+
+class LightViewController: UIViewController {
+    @IBOutlet var lightBtn:UIButton!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
