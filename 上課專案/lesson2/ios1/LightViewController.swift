c9d53fc78812aa33450ec841205989c7421f6180
徐國堂 <roberthus2003@gmail.com>
2019-04-15 21:58:33 +0800

    modify

--- 上課專案/lesson2/ios1/LightViewController.swift
+++ 上課專案/lesson2/ios1/LightViewController.swift
@@ -7,25 +7,31 @@
 //
 
 import UIKit
+import Firebase
 
 class LightViewController: UIViewController {
     @IBOutlet var lightBtn:UIButton!;
+    var relayRef:DatabaseReference!;
+    
+    override func awakeFromNib() {
+        super.awakeFromNib();
+        relayRef = Database.database().reference(withPath: "Relay")
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let relayHandler = relayRef.observe(.value){
+            (snapShot:DataSnapshot) in
+            let relayValue = snapShot.value as? [String:Bool]
+            if relayValue != nil{
+                print(relayValue!["D1"]!)
+            }
+            
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
