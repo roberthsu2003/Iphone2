86357e1635248ef991fb60b33f467ca988a87b85
徐國堂 <roberthus2003@gmail.com>
2019-04-22 20:35:27 +0800

    modify

--- 上課專案/lesson4/lesson4/ViewController.swift
+++ 上課專案/lesson4/lesson4/ViewController.swift
@@ -12,7 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("storyboard的view已經ready,可以使用了");
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view將出現了");
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view已經出現");
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated);
+        print("view要離開")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view已經離開")
     }
 
 
