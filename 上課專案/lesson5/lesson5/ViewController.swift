a1d7600687a02d5d0e8e65e7fa7e89bd6b43e246
徐國堂 <roberthus2003@gmail.com>
2019-04-26 21:49:18 +0800

    modify

--- 上課專案/lesson5/lesson5/ViewController.swift
+++ 上課專案/lesson5/lesson5/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
+    @IBOutlet var tableView:UITableView!;
+    
     let presidentsRef = Database.database().reference(withPath: "presidents")
     var presidents = [[String:String]]();
     
@@ -21,14 +23,38 @@ class ViewController: UIViewController {
            self.presidents = snapshot.value as! [[String:String]]
             print("下載完畢")
             print(self.presidents);
-            //我的資料下載完畢
+            //我的資料下載完畢,請tableView重新整理資料
+            self.tableView.reloadData();
         }
         
         print("尚未下載完畢")
         print(self.presidents);
         
+        //tableView.dataSource = self;
+        //tableView.delegate = self;
     }
 
 
+}
+
+extension ViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return (presidents.count == 0) ? 1 : presidents.count ;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
+        if presidents.count == 0 {
+            cell.textLabel!.text = "資料正在下載中";
+        }else{
+            let president = presidents[indexPath.row]
+            cell.textLabel!.text = president["name"]
+            cell.detailTextLabel!.text = president["url"];
+            
+        }
+        
+        
+        return cell;
+    }
 }
 
