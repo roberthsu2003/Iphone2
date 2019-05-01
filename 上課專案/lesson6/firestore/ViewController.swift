ff927bc682620b915121f166b57bb051bfcbc5a6
徐國堂 <roberthus2003@gmail.com>
2019-05-01 20:34:16 +0800

    modify

--- 上課專案/lesson6/firestore/ViewController.swift
+++ 上課專案/lesson6/firestore/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UITableViewController {
-
+    let firestore = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let presidentsCols = firestore.collection("presidents")
+        presidentsCols.addSnapshotListener { (presidentsSnapshot:QuerySnapshot?, error:Error?) in
+            if error != nil {
+                print("error:\(error!.localizedDescription)");
+                return;
+            }
+            
+            if presidentsSnapshot!.isEmpty{
+                self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "上傳資料", style: .plain, target: self, action: #selector(self.importData(_:)))
+            }else{
+                print("集合已經有資料");
+            }
+            
+            
+        }
     }
 
-
+    @objc func importData(_ sender:UIBarButtonItem){
+        print("上傳資料");
+    }
 }
 
