37edad22c1e0630e1a248d46482eb11a58a8c88a
徐國堂 <roberthus2003@gmail.com>
2019-05-20 21:57:10 +0800

    modify

--- 上課專案/lesson13/analytic/ViewController.swift
+++ 上課專案/lesson13/analytic/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //自訂的 custom_event
+        Analytics.logEvent(AnalyticsEventSelectContent, parameters: [
+            AnalyticsParameterItemID: "id-\(title!)" as NSObject,
+            AnalyticsParameterItemName: title! as NSObject,
+            AnalyticsParameterContentType:"cont" as NSObject,
+            ])
     }
 
 
