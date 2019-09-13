00be285c02993a21dc20cab6c678d77e1e11f742
徐國堂 <roberthus2003@gmail.com>
2019-09-13 14:50:09 +0800

    modify

--- 上課專案/lesson6/Pods/Target Support Files/Pods-firestore/Pods-firestore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_firestore : NSObject
-@end
-@implementation PodsDummy_Pods_firestore
-@end
--- 上課專案/lesson7/Pods/Target Support Files/Pods-cities/Pods-cities-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cities : NSObject
-@end
-@implementation PodsDummy_Pods_cities
-@end
