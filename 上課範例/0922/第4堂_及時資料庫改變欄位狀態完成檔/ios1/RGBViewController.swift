d11128c329e60ad773b5cd6adf4779545f1f8163
徐國堂 <roberthus2003@gmail.com>
2019-09-22 14:58:03 +0800

    modify

--- 上課範例/0922/第4堂_及時資料庫改變欄位狀態完成檔/ios1/RGBViewController.swift
+++ 上課範例/0922/第4堂_及時資料庫改變欄位狀態完成檔/ios1/RGBViewController.swift
@@ -15,6 +15,16 @@ class RGBViewController: UIViewController {
         super.viewDidLoad()
         colorPickerView.frame = view.frame;
         colorPickerView.color = UIColor.green;
+        colorPickerView.translatesAutoresizingMaskIntoConstraints = false;
+        let colorPickerViewConstraints = [
+        colorPickerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+        colorPickerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+        colorPickerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+        colorPickerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
+        ]
+        
+        NSLayoutConstraint.activate(colorPickerViewConstraints)
+        
         self.view.addSubview(colorPickerView)
        
     }
