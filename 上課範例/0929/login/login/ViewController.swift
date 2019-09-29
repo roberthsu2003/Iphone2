34f5edf98802807d553aa2f7b1b0f17ccc4c49de
徐國堂 <roberthus2003@gmail.com>
2019-09-29 15:15:15 +0800

    modify

--- 上課範例/0929/login/login/ViewController.swift
+++ 上課範例/0929/login/login/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    var databaseRef:DatabaseReference = Database.database().reference(withPath: "DHT")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,6 +21,23 @@ class ViewController: UIViewController {
         
     }
 
-
+    @IBAction func userClickBottomBtn(_ sender:UIButton){
+        let celsiusRef = databaseRef.child("Celsius")
+        celsiusRef.setValue("testValue"){
+            (error:Error?, dataRef:DatabaseReference) in
+            var message:String = "";
+            if let error = error{
+                print("user=\(error.localizedDescription)");
+                message = error.localizedDescription
+            }else{
+                message = "資料讀取正常"
+            }
+            
+            let alertController = UIAlertController(title: "請取狀態", message: message, preferredStyle: .alert);
+            let alertAction = UIAlertAction(title: "ok", style: .default, handler: nil);
+            alertController.addAction(alertAction);
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
 }
 
