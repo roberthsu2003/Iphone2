fe9094195944f4c374a30e99745d82abcd279a21
徐國堂 <roberthus2003@gmail.com>
2019-10-06 15:33:07 +0800

    modify

---
+++ 上課範例/1006/json/json/SubViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SubViewController.swift
+//  json
+//
+//  Created by Robert on 2019/10/6.
+//  Copyright © 2019 ios1. All rights reserved.
+//
+
+import UIKit
+
+class SubViewController: UIViewController {
+    var pageViewController:UIPageViewController!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        self.addChild(pageViewController);
+        self.view.addSubview(pageViewController.view);
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil);
+        let contentViewController = storyboard.instantiateViewController(identifier: "content");
+        pageViewController.setViewControllers([contentViewController], direction: .forward, animated: false, completion: nil);
+        
+    }
+    
+
+   
+}
