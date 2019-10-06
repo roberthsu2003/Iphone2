1caa172f3a151046955a56e49366bf194b00d8b3
徐國堂 <roberthus2003@gmail.com>
2019-10-06 16:46:58 +0800

    modify

--- 上課範例/1006/fireStore/fireStore/ViewController.swift
+++ 上課範例/1006/fireStore/fireStore/ViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func importData(_ sender:UIBarButtonItem){
+        print("匯入資料至Firebase");
+    }
 }
 
