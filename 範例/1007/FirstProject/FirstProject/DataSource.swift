e3ff37466f78b7eb350cccf2dd57a7c03ac0ad7e
徐國堂 <roberthus2003@gmail.com>
2019-10-07 21:40:21 +0800

    modify

---
+++ 範例/1007/FirstProject/FirstProject/DataSource.swift
@@ -0,0 +1,18 @@
+//
+//  DataSource.swift
+//  FirstProject
+//
+//  Created by Robert on 2019/10/7.
+//  Copyright © 2019 ios1. All rights reserved.
+//
+
+import Foundation
+class DataSource{
+    static let main:DataSource = {
+        return DataSource();
+    }();
+    
+    private init(){
+        
+    }
+}
