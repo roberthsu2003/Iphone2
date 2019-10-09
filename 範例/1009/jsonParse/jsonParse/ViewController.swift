fded81835d75a773e4f7254bbf297dcad07f643d
Robert Hsu <roberthsu2003@gmail.com>
2019-10-09 21:06:06 +0800

    modify

---
+++ 範例/1009/jsonParse/jsonParse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jsonParse
+//
+//  Created by 徐國堂 on 2019/10/9.
+//  Copyright © 2019 徐國堂. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let urlPath = "https://iostest-64ed7.web.app/gjun.json"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
