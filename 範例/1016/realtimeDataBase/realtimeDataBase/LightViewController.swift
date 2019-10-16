afb7ece59f99cde07f10d45d2add1d610322d863
Robert Hsu <roberthsu2003@gmail.com>
2019-10-16 20:42:48 +0800

    modify

--- 範例/1016/realtimeDataBase/realtimeDataBase/LightViewController.swift
+++ 範例/1016/realtimeDataBase/realtimeDataBase/LightViewController.swift
@@ -18,14 +18,8 @@ class LightViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func userChangeLight(_ sender:UIButton){
+        print("user change");
     }
-    */
 
 }
