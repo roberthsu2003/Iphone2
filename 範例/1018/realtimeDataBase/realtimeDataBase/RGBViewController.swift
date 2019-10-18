60d04cbbaeb9a0f3b4241c6160c28beb52c43dbd
Robert Hsu <roberthsu2003@gmail.com>
2019-10-18 21:11:27 +0800

    modify

--- 範例/1018/realtimeDataBase/realtimeDataBase/RGBViewController.swift
+++ 範例/1018/realtimeDataBase/realtimeDataBase/RGBViewController.swift
@@ -10,10 +10,37 @@ import UIKit
 import Color_Picker_for_iOS
 
 class RGBViewController: UIViewController {
-
+    var colorPickerView = HRColorPickerView();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        colorPickerView.color = UIColor.blue;
+        self.view.addSubview(colorPickerView)
+        /*
+        colorPickerView.frame = view.frame;
+        colorPickerView.frame.origin.y = 20;
+ */
+        colorPickerView.translatesAutoresizingMaskIntoConstraints = false;
+        let guide = view.safeAreaLayoutGuide
+        let topConstraint = colorPickerView.topAnchor.constraint(equalTo: guide.topAnchor, constant: 0)
+        let bottomConstraint = colorPickerView.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: 0)
+        
+        let leadConstraint = colorPickerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0)
+        
+        let trailConstraint = colorPickerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
+        
+        NSLayoutConstraint.activate([
+            topConstraint,
+            bottomConstraint,
+            leadConstraint,
+            trailConstraint
+        ])
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
 }
