7fb979d24ab42757ebd9745592e788b715fd2882
Robert Hsu <roberthsu2003@gmail.com>
2019-10-20 11:02:16 +0800

    modify

---
+++ 上課範例/1020/firestore_project1/firestore_project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firestore_project1
+//
+//  Created by 徐國堂 on 2019/10/20.
+//  Copyright © 2019 徐國堂. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
