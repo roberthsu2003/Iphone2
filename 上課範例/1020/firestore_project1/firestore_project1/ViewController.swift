e6f830447765fbf9dcc413ae84c15f654f7cef1b
Robert Hsu <roberthsu2003@gmail.com>
2019-10-20 12:18:13 +0800

    modify

--- 上課範例/1020/firestore_project1/firestore_project1/ViewController.swift
+++ 上課範例/1020/firestore_project1/firestore_project1/ViewController.swift
@@ -14,13 +14,20 @@ class ViewController: UITableViewController {
     //使用closure的執行，必需明確宣告資料類型
     //在closure內要傳出資料
     
-    lazy var presidents:[[String:String]] = {        
-        return [[String:String]]();
+    lazy var presidents:[[String:String]] = {
+        let path = Bundle.main.path(forResource: "PresidentList", ofType: "plist")
+        guard let rootDictionary = NSDictionary(contentsOfFile: path!) as? [String:Any] else{
+            print("解析資料有問題")
+            return [[String:String]]()
+        }
+        
+        let presidents = rootDictionary["presidents"] as! [[String:String]]
+        return presidents;
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("count:\(self.presidents.count)")
+        print("count:\(presidents)")
     }
 
 
