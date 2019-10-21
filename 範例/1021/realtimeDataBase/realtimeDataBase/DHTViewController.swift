fab2a31de57b59dbb962dd9b916a6e82f9abcc2d
Robert Hsu <roberthsu2003@gmail.com>
2019-10-21 20:26:16 +0800

    modify

---
+++ 範例/1021/realtimeDataBase/realtimeDataBase/DHTViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DHTViewController.swift
+//  realtimeDataBase
+//
+//  Created by 徐國堂 on 2019/10/21.
+//  Copyright © 2019 徐國堂. All rights reserved.
+//
+
+import UIKit
+
+class DHTViewController: UIViewController {
+    @IBOutlet var HumidityField:UITextField!
+    @IBOutlet var FahrenheitField:UITextField!
+    @IBOutlet var FahrenheitIndexField:UITextField!
+    @IBOutlet var CelsiusField:UITextField!
+    @IBOutlet var CelsiusIndexField:UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
