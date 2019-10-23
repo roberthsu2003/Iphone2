c15f25ddc7d0c0bd3c966f2ceb8ad3518500b8bc
Robert Hsu <roberthsu2003@gmail.com>
2019-10-23 19:52:15 +0800

    modify

---
+++ 第12堂_新增和顯示資料至NoSQL資料庫/第12堂新增和顯示資料至NoSQL資料庫/ios1/EditViewController.swift
@@ -0,0 +1,52 @@
+//
+//  EditViewController.swift
+//  ios1
+//
+//  Created by teacher on 2018/6/11.
+//  Copyright © 2018年 robert. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class EditViewController: UITableViewController {
+    var queryDocumentSnapshot:QueryDocumentSnapshot!;
+    @IBOutlet var nameField:UITextField!;
+    @IBOutlet var urlField:UITextField!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("documentId:\(queryDocumentSnapshot.documentID)");
+        let presidents = queryDocumentSnapshot.data();
+        nameField.text = presidents["name"] as? String;
+        urlField.text = presidents["url"] as? String;
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func userStopEditing(_ sender:UITextField){
+        print("stop Editting");
+    }
+    
+    @IBAction func userStopEditingAndExit(_ sender:UITextField){
+        print("stop Eidtting and Exit");
+    }
+    
+    @IBAction func userPressDone(_ sender:UIBarButtonItem){
+        print("press Done");
+        Firestore.firestore().collection("presidents").document(queryDocumentSnapshot.documentID).updateData([
+            "name":nameField.text!,
+            "url":urlField.text!
+            ]);
+        
+        navigationController!.popViewController(animated: true);
+    }
+
+    
+
+}
