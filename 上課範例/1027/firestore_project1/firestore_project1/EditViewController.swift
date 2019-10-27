c72bc4602e2d5627478d57189487c40692c45352
Robert Hsu <roberthsu2003@gmail.com>
2019-10-27 16:22:14 +0800

    modify

--- 上課範例/1027/firestore_project1/firestore_project1/EditViewController.swift
+++ 上課範例/1027/firestore_project1/firestore_project1/EditViewController.swift
@@ -13,23 +13,34 @@ class EditViewController: UITableViewController {
     @IBOutlet var nameField:UITextField!
     @IBOutlet var urlField:UITextField!
     var president:QueryDocumentSnapshot!
+    let firestore = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let presidentDic = president.data()
         print("presidentDic:\(presidentDic)")
+        
+        nameField.text = presidentDic["name"] as? String;
+        urlField.text = presidentDic["url"] as? String;
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func userPressUpdate(_ sender:UIBarButtonItem){
+        print("press update")
+        let documentID = president.documentID;
+        firestore.collection("presidents").document(documentID).updateData( [
+                       "name":nameField.text!,
+                       "url":urlField.text!,
+                       "time":Date().timeIntervalSince1970
+        ]) { (error:Error?) in
+            guard error == nil else{
+                print("更新失敗");
+                return
+            }
+            
+            print("更新成功");
+            self.navigationController?.popToRootViewController(animated: true)
+        }
     }
-    */
-
 }
