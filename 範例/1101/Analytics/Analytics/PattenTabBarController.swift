2b27d0ee4246624e1dbc65ad57a20f40325b90d7
Robert Hsu <roberthsu2003@gmail.com>
2019-11-01 19:43:14 +0800

    modify

---
+++ 範例/1101/Analytics/Analytics/PattenTabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  PattenTabBarController.swift
+//  Analytics
+//
+//  Created by 徐國堂 on 2019/11/1.
+//  Copyright © 2019 徐國堂. All rights reserved.
+//
+
+import UIKit
+
+class PattenTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
