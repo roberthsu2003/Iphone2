e00f8575e45a94c4935644cb67518573e49b0b81
Robert Hsu <roberthsu2003@gmail.com>
2019-11-03 15:49:42 +0800

    modify

--- 上課範例/1103/googleLogin_another/googleLogin/PickerSexViewController.swift
+++ 上課範例/1103/googleLogin_another/googleLogin/PickerSexViewController.swift
@@ -9,22 +9,26 @@
 import UIKit
 
 class PickerSexViewController: UIViewController {
-
+    var sexs = [ "Male", "Female"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension PickerSexViewController:UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        
+    }
+}
 
+extension PickerSexViewController:UIPickerViewDelegate{
+    
 }
