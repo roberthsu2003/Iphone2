f44943c6319df7f9c3eeef299c8068eab57b439e
Robert Hsu <roberthsu2003@gmail.com>
2019-11-06 18:55:38 +0800

    modify

---
+++ 範例/1106/CitysImage/CitysImage/DataSource.swift
@@ -0,0 +1,94 @@
+//
+//  DataSource.swift
+//  CitysImage
+//
+//  Created by 徐國堂 on 2019/11/4.
+//  Copyright © 2019 徐國堂. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class DataSource{
+    var firestore = Firestore.firestore()
+    var storage = Storage.storage()
+    var createUpLoadButton:(() -> Void)!
+    
+    static var dataSource:DataSource = {
+        print("執行");
+        return DataSource();
+    }()
+    
+    private init(){
+        checkDataInFirestore()
+    }
+    
+    func checkDataInFirestore(){
+        firestore.collection("citys").addSnapshotListener { (snapshot:QuerySnapshot?, error:Error?) in
+            guard error == nil else{
+                print("error:\(error!.localizedDescription)");
+                return
+            }
+            
+            guard let snapshot = snapshot else{
+                print("snapshot是nil");
+                return
+            }
+            
+            if snapshot.isEmpty{
+                
+                print("snapshot是empty");
+                if self.createUpLoadButton != nil {
+                    self.createUpLoadButton();
+                }
+                
+            }
+            
+            
+        }
+    }
+    
+    func uploadDataToFireStore(){
+        print("uploadDataToFireStore");
+        guard let cityListPath = Bundle.main.path(forResource: "citylist", ofType: "plist") else{
+            print("解析citylist路徑出錯");
+            return
+        }
+        
+        guard let cityList = NSArray(contentsOfFile: cityListPath) as? [[String:String]] else{
+            print("解析citylist的陣列出錯");
+            return
+        }
+        let batch = firestore.batch();
+        for cityItem in cityList{
+            //開始上傳資料
+            let documentRef = firestore.collection("citys").document();
+            batch.setData(cityItem, forDocument: documentRef)
+        }
+        
+        batch.commit { (error:Error?) in
+            guard error == nil else{
+                print("batch commit出錯:\(error!.localizedDescription)");
+                return
+            }
+            
+            print("資料已經全部上傳至firestore citys");
+            let cityImagesRef = self.storage.reference(withPath: "cityImages")
+            cityImagesRef.getData(maxSize: 1024*1024) { (data:Data?, error:Error?) in
+                guard data != nil else{
+                    print("data是nil");
+                    //出現上傳圖片的按鈕
+                    return;
+                }
+                
+                guard error == nil else{
+                    print("error:\(error!.localizedDescription)");
+                    return
+                }
+                
+            
+            }
+        
+        }
+    }
+}
---
+++ 範例/1106/CitysImage/CitysImage/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  CitysImage
+//
+//  Created by 徐國堂 on 2019/11/4.
+//  Copyright © 2019 徐國堂. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         DataSource.dataSource.createUpLoadButton = {
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "上傳資料", style: .plain, target: self, action: #selector(self.uploadData(_:)))
+          }
+    }
+    
+   
+    @objc func uploadData(_ sender:UIBarButtonItem){
+        DataSource.dataSource.uploadDataToFireStore()
+    }
+
+}
+
