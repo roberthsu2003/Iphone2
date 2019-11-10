263eb962dc6ddd17c69c7fdfd013cda8e1121095
Robert Hsu <roberthsu2003@gmail.com>
2019-11-10 12:36:33 +0800

    modify

--- 上課範例/1110/publicPrivateStorage/publicPrivateStorage/ViewController.swift
+++ 上課範例/1110/publicPrivateStorage/publicPrivateStorage/ViewController.swift
@@ -13,12 +13,16 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        userAuthentication();
+        if userAuthentication() {
+            //parse Plist in Documents
+            parsePlistInDocument()
+        }
     }
     
    
     
-    func userAuthentication(){
+    func userAuthentication() -> Bool{
+        var boolState = false;
         if Auth.auth().currentUser == nil {
                    //尚未認證
                    Auth.auth().signInAnonymously { (result:AuthDataResult?, error:Error?) in
@@ -26,21 +30,30 @@ class ViewController: UITableViewController {
                            print("anoonymously失敗");
                            return
                        }
-                    self.checkCityplistInDocuments()
+                        if self.checkCityplistInDocuments(){
+                            self.parsePlistInDocument()
+                        }
                     
                    }
                  
         }else{
         //已經認證
-          checkCityplistInDocuments()
+            if checkCityplistInDocuments() {
+                boolState = true;
+            }else{
+                boolState = false;
+            }
         }
+        
+       return boolState
     }
     
-    func checkCityplistInDocuments(){
+    func checkCityplistInDocuments() -> Bool{
+        var boolState:Bool = false;
         let fileManager = FileManager.default;
         guard let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else{
             print("沒有取得documentsURL");
-            return;
+            return false;
         }
         
         let plistURL = documentsURL.appendingPathComponent("citylist.plist")
@@ -49,12 +62,15 @@ class ViewController: UITableViewController {
             plistInStorageRef.getData(maxSize: 1*1024*1024) { (data:Data?, error:Error?) in
                 guard let plistData = data, error == nil else {
                     print("下載plist檔案有錯");
+                    boolState = false;
                     return
                 }
                 do{
                    try plistData.write(to: plistURL)
                 }catch let error as NSError{
                     print("寫入plist有錯:\(error.localizedDescription)")
+                    boolState = false;
+                    return
                 }
                 
                 let imagesDirectoryURL = documentsURL.appendingPathComponent("images", isDirectory: true)
@@ -63,13 +79,22 @@ class ViewController: UITableViewController {
                     
                 }catch let error as NSError{
                     print("建立images Directory有問題:\(error.localizedDescription)");
+                    boolState = false;
+                    return;
                 }
                 
+                boolState = true;
                 
             }
+            
         }
         
-        
+        return boolState;
+    }
+    
+    
+    func parsePlistInDocument(){
+        print("parsePlistInDocument")
     }
 
 
