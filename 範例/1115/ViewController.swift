67752ba98eaf6b9aea596d23fa869052f44516d0
Robert Hsu <roberthsu2003@gmail.com>
2019-11-15 21:21:40 +0800

    modify

--- 範例/1115/ViewController.swift
+++ 範例/1115/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     @IBOutlet var photoImageView:UIImageView!
@@ -18,7 +19,7 @@ class ViewController: UIViewController {
     @IBAction func userSelectedImage(_ sender:UIBarButtonItem){
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
             let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self;
+            imagePicker.delegate = self
             imagePicker.allowsEditing = false;
             imagePicker.sourceType = .photoLibrary
             present(imagePicker, animated: true, completion: nil)
@@ -26,7 +27,65 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onDeviceTextRecognition(_ sender:UIButton){
+        let vision = Vision.vision()
+        let textRecognizer = vision.onDeviceTextRecognizer()
+        guard let originImage = photoImageView.image else{
+            print("圖片有問題")
+            return
+        }
+         let visionImage = VisionImage(image: originImage)
+        textRecognizer.process(visionImage) { (text:VisionText?, error:Error?) in
+            guard let visionText = text, error == nil else{
+                print("文字辦識出問題:\(error!.localizedDescription)");
+                return
+            }
+            
+            //print("visionText.text:\(visionText.text)")
+            //print("visionText.blocks的數量:\(visionText.blocks.count)")
+            func subtractTextAndValidate(source:String) -> Int?{
+               
+                let startIndex = source.index(source.endIndex, offsetBy: -4)
+                let endIndex = source.index(before: source.endIndex)
+                let numberString = source[startIndex...endIndex]
+                print(numberString)
+                guard let cardNum = Int(numberString) else{
+                    return nil;
+                }
+                return cardNum;
+            }
+            
+            for textBlock in visionText.blocks{
+                let text = textBlock.text
+                
+                switch(text.count){
+                
+                case 6...8:
+                    
+                    if let carNumber = subtractTextAndValidate(source: text){
+                        print(text, terminator:" :");
+                        print("車牌號碼後4碼:\(carNumber)");
+                    }else{
+                        print(text, terminator:" :");
+                        print("不是車牌號碼)");
+                    }
+                    
+                  
+                    
+                default:
+                        
+                        print(text, terminator:":");
+                        print("不是車牌號碼")
+                }
+
+ 
+                
+            }
+            
+            
+            
+            
         
+      }
     }
     
     @IBAction func onCloundTextRecognition(_ sender:UIButton){
