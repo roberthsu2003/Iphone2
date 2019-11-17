b3e4c06682d6d48f3b62a2c79e5acf1049fa6fc4
Robert Hsu <roberthsu2003@gmail.com>
2019-11-17 16:36:05 +0800

    Update ViewController.swift

--- 上課範例/1117/ViewController.swift
+++ 上課範例/1117/ViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     @IBOutlet var photoImageView:UIImageView!
     @IBOutlet var messageOfBarCodeRecognizer:UITextView!
+    lazy var vision = Vision.vision()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -24,6 +26,42 @@ class ViewController: UIViewController {
             imagePickerController.sourceType = .photoLibrary
             present(imagePickerController, animated: true, completion: nil)
         }
+    }
+    
+    @IBAction func barCodeScan(_ sender:UIButton){
+        //let formats = VisionBarcodeFormat(arrayLiteral: VisionBarcodeFormat.code39,VisionBarcodeFormat.qrCode)
+        let formats = VisionBarcodeFormat.all
+        let barcodeOptions = VisionBarcodeDetectorOptions(formats: formats)
+        let barcodeDetector = vision.barcodeDetector(options: barcodeOptions)
+        guard let originalImage = photoImageView.image else{
+            showMessage(message: "沒有選擇圖片")
+            return
+        }
+        let visionImage = VisionImage(image:originalImage)
+        barcodeDetector.detect(in: visionImage) { (visionB:[VisionBarcode]?, error:Error?) in
+            guard error == nil else{
+                self.showMessage(message: "barCode辨識出錯:\(error!.localizedDescription)")
+                return
+            }
+            
+            guard let visionBarcodes = visionB,visionBarcodes.count > 0 else{
+                self.showMessage(message: "barCode辨識出錯")
+                return
+            }
+            
+            for vistionBarcode in visionBarcodes{
+                print(vistionBarcode.displayValue!);
+                print("......................");
+            }
+            
+        }
+    }
+    
+    func showMessage(message:String){
+        let alertController = UIAlertController(title: "錯誤", message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
     }
     
 }
