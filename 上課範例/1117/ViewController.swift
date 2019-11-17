2081a866c539389c67059b7d2a043419e1acc43f
Robert Hsu <roberthsu2003@gmail.com>
2019-11-17 10:45:23 +0800

    modify

---
+++ 上課範例/1117/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  textRecognizer1
+//
+//  Created by 徐國堂 on 2019/11/17.
+//  Copyright © 2019 徐國堂. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var photoImageView:UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func selectedImage(_ sender:UIBarButtonItem){
+        
+    }
+}
+
+
