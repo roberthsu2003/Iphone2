ce000946aff2c81360e4b0bb6748d5f736fa59e0
Robert Hsu <roberthsu2003@gmail.com>
2019-11-22 19:23:17 +0800

    modify

---
+++ 範例/1122/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  faceDetect
+//
+//  Created by 徐國堂 on 2019/11/22.
+//  Copyright © 2019 徐國堂. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var photoImageView:UIImageView!;
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let eyeView = UIView()
+        eyeView.frame = CGRect(x: 0, y: 100, width: 100, height: 20)
+        eyeView.backgroundColor = UIColor.darkGray
+        view.addSubview(eyeView)
+    }
+
+
+}
+
