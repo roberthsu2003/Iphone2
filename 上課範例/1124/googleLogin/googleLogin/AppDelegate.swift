0a87f6390eb32151b1b6915a9e9934b3b2b8546d
Robert Hsu <roberthsu2003@gmail.com>
2019-11-24 16:13:16 +0800

    modify

--- 上課範例/1124/googleLogin/googleLogin/AppDelegate.swift
+++ 上課範例/1124/googleLogin/googleLogin/AppDelegate.swift
@@ -21,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         let auth = Auth.auth();
         do{
             try auth.signOut()
