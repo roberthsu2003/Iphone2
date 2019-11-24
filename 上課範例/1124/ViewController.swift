f5acfecd16053d6e2687d091c06bc98893c017c4
Robert Hsu <roberthsu2003@gmail.com>
2019-11-24 10:59:52 +0800

    modify

--- 上課範例/1124/ViewController.swift
+++ 上課範例/1124/ViewController.swift
@@ -9,18 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var  photoImageView:UIImageView!
+    @IBOutlet var messageTextView:UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        /*
         let faceView = FaceView()
         faceView.frame = CGRect(x: 0, y: 100, width: 50, height: 50)
         faceView.backgroundColor = UIColor.red
         faceView.layer.borderWidth = 2
         faceView.layer.borderColor = UIColor.red.cgColor
         faceView.layer.backgroundColor = UIColor.clear.cgColor
-        view.addSubview(faceView)       
+        view.addSubview(faceView)
+ */
+        
+    }
+    
+    @IBAction func userSelectedImage(_ sender:UIBarButtonItem){
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self;
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .photoLibrary
+            present(imagePicker, animated: true, completion: nil);
+        }
     }
 
 
+}
+
+extension ViewController:UINavigationControllerDelegate,UIImagePickerControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+                      photoImageView.image = selectedImage;
+                      photoImageView.clipsToBounds = true;
+                  }
+                  messageTextView.text = ""
+                  dismiss(animated: true, completion: nil);
+    }
 }
 
