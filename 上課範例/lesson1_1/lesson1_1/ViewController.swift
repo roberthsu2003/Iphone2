3982225b15e2e472687c5f24da7abf80d51a4ff2
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-16 21:13:05 +0800

    modify

--- 上課範例/lesson1_1/lesson1_1/ViewController.swift
+++ 上課範例/lesson1_1/lesson1_1/ViewController.swift
@@ -8,16 +8,19 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var states:[String:[String]]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         if let targetPath = Bundle.main.path(forResource: "statedictionary", ofType: "plist"){
             
             if let states = NSDictionary(contentsOfFile: targetPath) as? [String:[String]]{
-                print(states["Alabama"]!)
+                self.states = states
             }
             
         }
+        
+        print(states["Alaska"]!)
     }
 
 
