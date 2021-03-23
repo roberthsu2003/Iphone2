6330fc0ac994508c1bf14ff6c2bbe1e9262ff769
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-23 21:44:46 +0800

    modify

---
+++ 上課範例/lesson3_1/lesson3_1/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  lesson3_1
+//
+//  Created by 徐國堂 on 2021/3/23.
+//
+
+import UIKit
+
+struct Region:Codable{
+    let areas:[String]
+}
+
+class ViewController: UIViewController {
+    let areasHttpString = "https://flask-robert.herokuapp.com/youbike/"
+    var areas:[String]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: areasHttpString)!
+        let downloadTask = URLSession.shared.downloadTask(with: url){
+            (saveURL:URL?, response:URLResponse?, error:Error?) in
+            guard let saveURL=saveURL, let response = response, error == nil else{
+                print("下載失敗")
+                return
+            }
+            
+            guard (response as! HTTPURLResponse).statusCode == 200 else{
+                print("狀態不是200")
+                return
+            }
+            
+            guard let data = try? Data(contentsOf: saveURL) else{
+                print("下載資料無法轉出")
+                return
+            }
+            
+            DispatchQueue.main.sync {
+                let jsonDecoder = JSONDecoder()
+                guard let region = try? jsonDecoder.decode(Region.self, from: data) else{
+                    print("jsonDecoder無法轉換")
+                    return
+                }
+                print("下載成功")
+                
+                self.areas = region.areas
+                print(self.areas!)
+            }
+        }
+        downloadTask.resume()
+    }
+
+
+}
+
