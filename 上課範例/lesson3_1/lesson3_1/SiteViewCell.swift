c89e2271ecfd620eb1af1eaaad959814be7632d5
roberthsu2003 <roberthsu2003@gmail.com>
2021-03-30 21:12:12 +0800

    modify

---
+++ 上課範例/lesson3_1/lesson3_1/SiteViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SiteViewCell.swift
+//  lesson3_1
+//
+//  Created by 徐國堂 on 2021/3/30.
+//
+
+import UIKit
+
+class SiteViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
