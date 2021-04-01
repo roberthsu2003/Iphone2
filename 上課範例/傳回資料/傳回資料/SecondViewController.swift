18583441778c61cdfdcb363ee4f7679f16f9965a
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-01 21:14:32 +0800

    modify

--- 上課範例/傳回資料/傳回資料/SecondViewController.swift
+++ 上課範例/傳回資料/傳回資料/SecondViewController.swift
@@ -1,33 +1,27 @@
 //
-//  FirstViewController.swift
+//  SecondViewController.swift
 //  傳回資料
 //
 //  Created by 徐國堂 on 2021/4/1.
 //
 
 import UIKit
-protocol FirstViewControlerDelegate:NSObject {
-    func dataPassBack(userName name:String,userPassword password:String)
-}
-class FirstViewController: UITableViewController {
+
+class SecondViewController: UITableViewController {
     @IBOutlet var nameField:UITextField!
     @IBOutlet var passwordField:UITextField!
-    weak var delegate:FirstViewControlerDelegate!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
     }
-    
+
     @IBAction func userOk(_ sender:UIBarButtonItem){
         
         let name = nameField.text!
         let password = passwordField.text!
-        delegate.dataPassBack(userName: name, userPassword: password)
+        
         navigationController?.popToRootViewController(animated: true)
-    }
-    
-    deinit{
-        print("FirstViewController記憶體被釋放了")
     }
 
 }
