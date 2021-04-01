292e35a6044aa327badf718b50ca16d425ff5ed1
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-01 19:26:30 +0800

    modify

---
+++ 上課範例/傳回資料/傳回資料/FirstViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.swift
+//  傳回資料
+//
+//  Created by 徐國堂 on 2021/4/1.
+//
+
+import UIKit
+
+class FirstViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }  
+
+}
---
+++ 上課範例/傳回資料/傳回資料/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  傳回資料
+//
+//  Created by 徐國堂 on 2021/4/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
