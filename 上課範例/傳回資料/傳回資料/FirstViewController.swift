2478ba797bbb6d9d163c8500575ac44e76f64189
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-01 19:57:55 +0800

    modify

--- 上課範例/傳回資料/傳回資料/FirstViewController.swift
+++ 上課範例/傳回資料/傳回資料/FirstViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class FirstViewController: UITableViewController {
-
+    @IBOutlet var nameField:UITextField!
+    @IBOutlet var passwordField:UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    }  
+    }
+    
+    @IBAction func userOk(_ sender:UIBarButtonItem){
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    deinit{
+        print("FirstViewController記憶體被釋放了")
+    }
 
 }
