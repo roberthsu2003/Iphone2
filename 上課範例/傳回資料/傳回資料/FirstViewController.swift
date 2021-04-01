6fcef7e2105a924627fb902aaf8363830c318e51
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-01 20:21:45 +0800

    modify

--- 上課範例/傳回資料/傳回資料/FirstViewController.swift
+++ 上課範例/傳回資料/傳回資料/FirstViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
-
+protocol FirstViewControlerDelegate:NSObject {
+    func dataPassBack(userName name:String,userPassword password:String)
+}
 class FirstViewController: UITableViewController {
     @IBOutlet var nameField:UITextField!
     @IBOutlet var passwordField:UITextField!
+    weak var delegate:FirstViewControlerDelegate!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +20,9 @@ class FirstViewController: UITableViewController {
     
     @IBAction func userOk(_ sender:UIBarButtonItem){
         navigationController?.popToRootViewController(animated: true)
+        let name = nameField.text!
+        let password = passwordField.text!
+        delegate.dataPassBack(userName: name, userPassword: password)
     }
     
     deinit{
