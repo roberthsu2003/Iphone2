5dd6fcefed47ed1ef3d8c932d711907b42196bfd
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-01 20:38:27 +0800

    modify

--- 上課範例/傳回資料/傳回資料/ViewController.swift
+++ 上課範例/傳回資料/傳回資料/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "FIRST"{
+            let firstViewController = segue.destination as! FirstViewController
+            firstViewController.delegate = self
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+extension ViewController:FirstViewControlerDelegate{
+    func dataPassBack(userName name:String,userPassword password:String){
+        print("使用者名稱是:\(name)")
+        print("使用者密碼是:\(password)")
+    }
 }
 
