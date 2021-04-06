5808bdd02ffb94fbe0bfbfd30feb84eab6847a2c
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-06 19:37:58 +0800

    modify

--- 上課範例/downloadImage/downloadImage/DownloadViewController.swift
+++ 上課範例/downloadImage/downloadImage/DownloadViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 class DownloadViewController: UIViewController {
     @IBOutlet var cityImageView:UIImageView!
     @IBOutlet var progressView:UIProgressView!
+    let pathString = "https://flask-robert.herokuapp.com/static/cityImage/Akihabara.jpg"
+    lazy var session:URLSession = {
+        let config = URLSessionConfiguration.ephemeral
+        config.allowsCellularAccess = true
+        config.allowsExpensiveNetworkAccess = true
+        let session = URLSession(configuration: config)
+        return session
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
