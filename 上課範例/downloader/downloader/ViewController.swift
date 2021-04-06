b7258c3efdbd8a68314115d978c7c6b1fa8d5fa5
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-06 21:22:47 +0800

    modify

--- 上課範例/downloader/downloader/ViewController.swift
+++ 上課範例/downloader/downloader/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let downloader:Downloader = {
+        let config = URLSessionConfiguration.ephemeral
+        config.allowsCellularAccess = true
+        config.allowsExpensiveNetworkAccess = true
+        return Downloader(config: config)
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doDownLoad(_ sender:UIButton){
+        let paths = [ "https://flask-robert.herokuapp.com/static/cityImage/Auckland.jpg",
+            "https://flask-robert.herokuapp.com/static/cityImage/Berlin.jpg",
+            "https://flask-robert.herokuapp.com/static/cityImage/Birmingham.jpg"
+        ]
     }
 
 
