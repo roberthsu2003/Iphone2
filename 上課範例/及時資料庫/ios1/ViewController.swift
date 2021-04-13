ffc8e395fe2cc22047ae3838ff9b41eb9fe01e9b
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-13 20:09:31 +0800

    modify

--- 上課範例/及時資料庫/ios1/ViewController.swift
+++ 上課範例/及時資料庫/ios1/ViewController.swift
@@ -12,22 +12,28 @@ class ViewController: UIViewController {
     var relayRef:DatabaseReference!
     override func awakeFromNib() {
         super.awakeFromNib()
-        //暱名登入
-        Auth.auth().signInAnonymously { (result:AuthDataResult?, error:Error?) in
-            guard let result = result, error == nil else{
-                return
-            }
-            let user = result.user
-           
-            if user.isAnonymous{
-                print(user.uid)
-            }
-            
-        }
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if Auth.auth().currentUser != nil {
+          print("登入完成")
+            print(Auth.auth().currentUser?.uid)
+        } else {
+            //暱名登入
+            Auth.auth().signInAnonymously { (result:AuthDataResult?, error:Error?) in
+                guard let result = result, error == nil else{
+                    return
+                }
+                let user = result.user
+               
+                if user.isAnonymous{
+                    print(user.uid)
+                }
+                
+            }
+        }
         relayRef = Database.database().reference(withPath: "relay/d1")
         relayRef.observe(.value) { (snapshot:DataSnapshot) in
             print("資料改變了")
