fc1a4dc59d9f66e90f9e145ce8114c48656b37d3
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-04 21:57:46 +0800

    modify

---
+++ 上課範例/storage/ios1/CityCell.swift
@@ -0,0 +1,13 @@
+//
+//  CityCell.swift
+//  ios1
+//
+//  Created by 徐國堂 on 2021/5/4.
+//
+
+import UIKit
+
+class CityCell: UICollectionViewCell {
+    @IBOutlet var cityImageView:UIImageView!
+    @IBOutlet var cityNameLabel:UILabel!
+}
