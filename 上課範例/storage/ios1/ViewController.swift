f67f517ab0a9b553690e846cb26c4cac22ebdedd
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-04 19:55:24 +0800

    modify

--- 上課範例/storage/ios1/ViewController.swift
+++ 上課範例/storage/ios1/ViewController.swift
@@ -9,18 +9,36 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
+    let firestore = Firestore.firestore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //登入動作
         if Auth.auth().currentUser == nil{
             print("nil")
             performSegue(withIdentifier: "goLoading", sender: nil)
         }else{
             print("login完成")
-            performSegue(withIdentifier: "goLoading", sender: nil)
+            //performSegue(withIdentifier: "goLoading", sender: nil)
+        }
+        
+        //檢查Firestore有沒有資料
+        firestore.collection("cities").getDocuments { (snapshot:QuerySnapshot?, error:Error?) in
+            guard let snapshot = snapshot, error == nil else{
+                print("取得documents失敗")
+                return
+            }
+            
+            if snapshot.isEmpty{
+                self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "匯入資料", style: .plain, target: self, action: #selector(self.uploadData(_:)))
+            }else{
+                print("已經有資料")
+            }
         }
     }
 
-
+    @objc func uploadData(_ sender:UIBarButtonItem){
+        print("上傳資料")
+    }
 }
 
