4147aec28c96738cefe4a73ddc0ec3f51286b3ca
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-06 21:10:18 +0800

    modify

---
+++ 上課範例/text_recognition部份檔/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ios1
+//
+//  Created by 徐國堂 on 2021/5/6.
+//
+
+import UIKit
+import MLKitTextRecognition
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
